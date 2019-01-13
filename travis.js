ef2e6bd94d802e9f328ceb123bf8431efe3ba830
al66 <andreas.leinen@team-ag.de>
2019-01-13 19:16:06 +0100

    configure travis

---
+++ travis.js
@@ -0,0 +1,6 @@
+process.env.URI = "bolt://127.0.0.1:7687";
+process.env.KAFKA_BROKER = "127.0.0.1:9092";
+process.env.REDIS_HOST = "127.0.0.1";
+process.env.REDIS_PORT = "6379";
+process.env.REDIS_AUTH = "";
+process.env.REDIS_DB = 0;
