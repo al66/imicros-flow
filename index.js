4ba5fc9b9fafa91948e75501806bb3f3cf5612f3
al66 <andreas.leinen@team-ag.de>
2019-01-13 19:26:47 +0100

    configure travis

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ module.exports = {
     Controller: require("./lib/flow.control"),
     Query: require("./lib/flow.query"),
     Listener: require("./lib/flow.listener"),
-    Worker: require("./lib/flow.worker"),
     Store: require("./lib/flow.store"),
     Middleware: require("./lib/flow.middleware"),
     StaticSubscriber: require("./lib/flow.static.subscriber"),
