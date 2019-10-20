3a83a844d20b8212b42553307e69a66794ee244d
al66 <andreas.leinen@imicros.de>
2019-10-20 12:16:08 +0200

    travis: correction due to neo4j service

--- travis.js
+++ travis.js
@@ -1,4 +1,4 @@
-process.env.URI = "bolt://127.0.0.1:7687";
+process.env.URI = "bolt://127.0.0.1:7474";
 process.env.KAFKA_BROKER = "kafka:9092";
 process.env.REDIS_HOST = "127.0.0.1";
 process.env.REDIS_PORT = "6379";
